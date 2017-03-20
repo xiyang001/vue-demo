5e8d71c51644f657186c5c19d7cef10ba071d6c3
zhiyuan.xi <zhiyuan.xi@wenba100.com>
2017-03-20 16:53:48 +0800

    修改文件路径

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -14,9 +14,10 @@ module.exports = {
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
+    /*publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+      : config.dev.assetsPublicPath*/
+    publicPath:'./'
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
